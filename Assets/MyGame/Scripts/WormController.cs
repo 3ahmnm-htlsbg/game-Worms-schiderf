f5615ac20f9abe1eda4f97c41e94b4f4fb8e11a9
schiderf <felix.schider02@htl-salzburg.ac.at>
2020-05-19 14:55:24 +0200

    Aufgabe der Stunde

---
+++ Assets/MyGame/Scripts/WormController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormController : MonoBehaviour
+{
+    public KeyCode jumpKey;
+    public KeyCode fowardKey;
+    public Rigidbody z;
+    public Vector3 x;
+    public Vector3 y;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(jumpKey))
+        {
+            Debug.Log("Jump taste wurde gedrückt");
+            z.AddForce(x);
+        }
+
+        if (Input.GetKeyDown(fowardKey))
+        {
+            Debug.Log("Forwärts taste wurde gedrückt");
+            z.AddForce(y);
+
+        }
+    }
+}
